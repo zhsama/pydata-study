94d451838be5cbea00ccd8d438a95f31153dd0f5
zhsama <a602693793@gmail.com>
2018-05-31 20:54:40 +0800

    0531 mid

--- 数据清洗和准备/7.1  处理缺失数据/滤除缺失数据.py
+++ 数据清洗和准备/7.1  处理缺失数据/滤除缺失数据.py
@@ -9,3 +9,11 @@ import pandas as pd
 import numpy as np
 from numpy import nan as na
 
+data = pd.Series([1, na, 3.5, na, 7])
+# print(data.dropna())  # 清除所有na值
+# print(data[data.notnull()])  # 等价
+
+# dataframe
+data = pd.DataFrame([[1., 6.5, 3.], [1., na, na], [na, na, na], [na, 6.5, 3.]])
+# print(data)
+print(data.dropna())
