3f08773c18a9b1afa7d2369709883a248ec5b87a
zhsama <a602693793@gmail.com>
2018-09-12 20:45:41 +0800

    0912 afternoon

--- 第 14 章  数据分析案例/14.2 MovieLens/计算评分分歧.py
+++ 第 14 章  数据分析案例/14.2 MovieLens/计算评分分歧.py
@@ -5,34 +5,40 @@
 # @Software: PyCharm
 
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 pd.options.display.max_rows = 10
 
 unames = ['user_id', 'gender', 'age', 'occupation', 'zip']
-users = pd.read_table('users.dat', sep='::', header=None, names=unames)
+users = pd.read_table('users.dat', sep="::", header=None, names=unames)
 rnames = ['user_id', 'movie_id', 'rating', 'timestamp']
-ratings = pd.read_table('ratings.dat', sep='::', header=None, names=rnames)
+ratings = pd.read_table('ratings.dat', sep="::", header=None, names=rnames)
 mnames = ['movie_id', 'title', 'genres']
-movies = pd.read_table('movies.dat', sep='::', header=None, names=mnames)
+movies = pd.read_table('movies.dat', sep="::", header=None, names=mnames)
 
-data = pd.merge(pd.merge(ratings, users), movies)
+data = pd.merge(pd.merge(users, ratings), movies)
+# print(data)
 
+# 计算平均分
 mean_ratings = data.pivot_table('rating', index='title', columns='gender', aggfunc='mean')
+# print(mean_ratings)
 
+# 获取影评>250的电影
 ratings_by_title = data.groupby('title').size()
+# print(ratings_by_title)
+active_title = ratings_by_title.index[ratings_by_title > 250]
+mean_ratings = mean_ratings.loc[active_title]
+print(mean_ratings)
 
-active_titles = ratings_by_title.index[ratings_by_title >= 250]
+# 获取差异最大的影评
+mean_ratings['diff'] = mean_ratings['F'] - mean_ratings['M']
+sorted_by_diff = mean_ratings.sort_values(by='diff', ascending=False)
+# print(sorted_by_diff[:10])
 
-mean_ratings = mean_ratings.loc[active_titles]
+# 查找分歧(按方差/标准差)
+ratings_std_by_title = data.groupby('title')['rating'].std()
+ratings_std_by_title = ratings_std_by_title.loc[active_title]
+print(ratings_std_by_title.sort_values(ascending=False)[:10])
 
-top_female_ratings = mean_ratings.sort_values(by='F', ascending=False)
-
-# 找出男性和女性观众分歧最大的电影
-# 方案1:给mean_ratings加上一个用于存放平均得分之差的列，并对其进行排序
-
-mean_ratings['diff'] = mean_ratings['M'] - mean_ratings['F']
-sorted_by_diff = mean_ratings.sort_values(by='diff')  # 按"diff"排序即可得到分歧最大且女性观众更喜欢的电影
-print(sorted_by_diff)
+# 根据女性评分降序排列
+# top_female_ratings = mean_ratings.sort_values(by='F', ascending=False)
+# print(top_female_ratings)
---
+++ 第 14 章  数据分析案例/14.3  1880-2010 年间全美婴儿姓名/us baby name.py
@@ -0,0 +1,11 @@
+# @Version : Python3.6
+# @Time    : 2018/9/12 19:35
+# @Author  : zhcf1ess
+# @File    : us baby name.py
+# @Software: PyCharm
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
